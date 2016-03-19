b091facf21929ce2eaae232923a6332a744e56e7
opsat73 <opsat7373@gmail.com>
2016-03-20 01:27:31 +0200

    feat(component) add sorting

--- components/menu/view/menu.php
+++ components/menu/view/menu.php
@@ -17,6 +17,19 @@
                         Bucket
                     </a>
                 </li>
+                <?php if ($show_sort_buttons): ?>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        <?php echo ($sort == ASC)?'Cheaper first': 'Expensive first'; ?>
+                        <span class="caret">
+
+                        </span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/<?php echo $category?>/ASC/1">Cheaper first</a></li>
+                        <li><a href="/<?php echo $category?>/DESC/1">Expensive first</a></li>
+                    </ul>
+                </li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
