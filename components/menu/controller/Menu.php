b959d285418486d92e3aa175068f88dfecad2fea
opsat73 <opsat7373@gmail.com>
2016-03-20 00:49:23 +0200

    feat(component) add menu component

---
+++ components/menu/controller/Menu.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: opsat73
+ * Date: 18.03.16
+ * Time: 22:11
+ */
+namespace components\menu\controller;
+use core\Controller;
+
+class Menu extends Controller
+{
+    public function actionDrawMenu () {
+
+    }
+}
---
+++ components/menu/view/menu.php
@@ -0,0 +1,23 @@
+<?php
+?>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="/0/ASC/1">
+                        List
+                        <span class="sr-only">
+                            (current)
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        Bucket
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
