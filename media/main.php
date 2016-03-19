d96d135adf3442d1db99ccd4d681e5aaa751930a
opsat73 <opsat7373@gmail.com>
2016-03-20 00:47:32 +0200

    feat(meida) plug in bootstrap add media files add main layout

---
+++ media/main.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/media/css/bootstrap.min.css" type="text/css" rel="stylesheet"/>
+    <link href="/media/css/bootstrap-theme.min.css" type="text/css" rel="stylesheet"/>
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet">
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700' rel='stylesheet' type='text/css'>
+    <link href="/media/css/theme.css" rel="stylesheet">
+
+</head>
+<body role="document">
+<?php echo $menu?>
+<?php echo $content ?>
+<script type="application/javascript" src="/media/js/jquery.min.js"></script>
+<script type="application/javascript" src="/media/js/bootstrap.min.js"></script>
+<script type="application/javascript" src="/media/js/jquery.hotkeys.js"></script>
+<script type="application/javascript" src="/media/js/bootstrap-wysiwyg.js"></script>
+
+</body>
+</html>
